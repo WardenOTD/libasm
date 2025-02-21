5dbf85d47cb06e8b477f29eef98b4ad246192786
Warden <warden@work.com>
2025-02-21 11:41:07 +0800

    changed comments to say sys_read instead of sys_write

--- ft_read.s
+++ ft_read.s
@@ -10,7 +10,7 @@ section .text
 
 ft_read:
 	mov rax, 0							; syscall number for sys_read is 0
-										; sys_write : rdi = fd, rsi = buf, rdx = count
+										; sys_read : rdi = fd, rsi = buf, rdx = count
 										; it's already correct so we don't mov anything
 	syscall								; call the kernel
 	test rax, rax						; test reg, reg == cmp reg, 0
@@ -20,7 +20,7 @@ ft_read:
 	ret
 
 error:
-	mov r8, rax							; save sys_write error to general purpose register
+	mov r8, rax							; save sys_read error to general purpose register
 	neg r8								; negate subtracts the value by 0, aka 2s complement
 										; i.e. 0 - -9 == 9
 	call __errno_location wrt ..plt		; __errno_location returns address of errno in c
