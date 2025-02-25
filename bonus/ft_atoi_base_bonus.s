b4302884e7141b8cdd57d6c901f71a7e45a1195d
Warden <warden@work.com>
2025-02-25 16:12:22 +0800

    added comments

--- bonus/ft_atoi_base_bonus.s
+++ bonus/ft_atoi_base_bonus.s
@@ -7,8 +7,8 @@ section .text
 ; rdi = *str, rsi = *base
 
 ft_atoi_base:
-	push rbp
-	mov rbp, rsp
+	push rbp						; align stack frame
+	mov rbp, rsp					; ^^
 	push rbx						; rbx must be pushed whenever used and restored after
 	xor rax, rax					; set rax to 0
 	mov rcx, -1						; set rcx to -1
@@ -46,9 +46,9 @@ check_base_dup:
 	jmp check_base_dup				; else loop
 
 interrupt:
-	cmp rcx, 1						; check base of len
+	cmp rcx, 1						; check len of base
 	jbe exit_err					; if 1 or less exit
-	mov rbx, rcx
+	mov rbx, rcx					; store len of *base in rbx
 
 ; ================================================================================= ;
 ; int	ft_atoi(char *str)
@@ -91,98 +91,93 @@ neg_equal:
 	jmp check_sign					; loop
 
 count:
-	xor r11, r11
+	xor r11, r11					; set r11 to 0, this count our digit len
 
 loop_actual_int_len:
-	cmp byte [rdi + rcx], 0
-	jz prepare_copy
-	cmp byte [rdi + rcx], 0x20
-	je prepare_copy
-	cmp byte [rdi + rcx], 0x09
-	je prepare_copy
-	cmp byte [rdi + rcx], 0x0A
-	je prepare_copy
-	cmp byte [rdi + rcx], 0x0B
-	je prepare_copy
-	cmp byte [rdi + rcx], 0x0C
-	je prepare_copy
-	cmp byte [rdi + rcx], 0x0D
-	je prepare_copy
-	inc rcx
-	inc r11
-	jmp loop_actual_int_len
+	cmp byte [rdi + rcx], 0			; compare to 0
+	jz prepare_copy					; if zero jump
+	cmp byte [rdi + rcx], 0x20		; compare to space
+	je prepare_copy					; if equal jump
+	cmp byte [rdi + rcx], 0x09		; compare to \t
+	je prepare_copy					; if equal jump
+	cmp byte [rdi + rcx], 0x0A		; compare to \n
+	je prepare_copy					; if equal jump
+	cmp byte [rdi + rcx], 0x0B		; compare to \v
+	je prepare_copy					; if equal jump
+	cmp byte [rdi + rcx], 0x0C		; compare to \f
+	je prepare_copy					; if equal jump
+	cmp byte [rdi + rcx], 0x0D		; compare to \r
+	je prepare_copy					; if equal jump
+	inc rcx							; ++rcx
+	inc r11							; ++r11
+	jmp loop_actual_int_len			; loop
 
 prepare_copy:
-	sub rcx, r11
-	dec r11
+	sub rcx, r11					; subtract rcx by r11 (return to start of digit)
+	dec r11							; --r11 (base conversion starts with 0 index)
 
 copy_actual_int:
-	cmp r11, 0
-	jb done
+	cmp r11, 0						; compare len of digit to 0
+	jb done							; if less than jump
 	mov r9b, byte [rdi + rcx]		; set r9b (8bit register of r9) to current byte in str
-	jmp init_find_index
+	jmp init_find_index				; jump
 
 init_find_index:
-	push rcx
-	mov rcx, -1
+	push rcx						; store rcx
+	mov rcx, -1						; set rcx as -1
 
 find_index:
-	inc rcx
-	cmp byte [rsi + rcx], 0
-	jz done
-	cmp r9b, [rsi + rcx]
-	jne find_index
-	mov r9, rcx
-	pop rcx
+	inc rcx							; ++rcx
+	cmp byte [rsi + rcx], 0			; compare to 0
+	jz done							; if zero jump
+	cmp r9b, [rsi + rcx]			; compare current with *base
+	jne find_index					; if not equal loop
+	mov r9, rcx						; set r9 as rcx
+	pop rcx							; restore rcx
 
 save_num:
-	push rax
-	mov rax, rbx
-	push r11
-	cmp r11, 0
-	jz last_digit
-	dec r11
+	push rax						; store rax
+	mov rax, rbx					; set rax as rbx
+	push r11						; store r11
+	cmp r11, 0						; compare r11 to 0
+	jz last_digit					; if zero jump
+	dec r11							; --r11
 
 mul_loop:
-	cmp r11, 0
-	je mul_loop_end
-	mul rbx
-	dec r11
-	jmp mul_loop
+	cmp r11, 0						; compare with 0
+	je mul_loop_end					; if equal break
+	mul rbx							; multiply rax by rbx
+	dec r11							; --r11
+	jmp mul_loop					; loop
 
 last_digit:
-	mov rax, 1
+	mov rax, 1						; set rax as 1
 
 mul_loop_end:
-	pop r11
-	push rax
-	push r9
-	pop rax
-	pop r9
-	mul r9
-	push rax
-	pop r9
-	pop rax
+	pop r11							; restore r11
+	push rax						; store rax
+	push r9							; store r9
+	pop rax							; restore rax (now rax is r9 because of FILO)
+	pop r9							; restore r9 (now r9 is rax)
+	mul r9							; multiply rax by r9
+	push rax						; store rax
+	pop r9							; restore r9 (now r9 is rax)
+	pop rax							; restore rax (c:139)
 	jc done							; if carry (overflow) exit with what we have
-	add rax, r9						; rax + r9, [10 + 1 == 11]
+	add rax, r9						; rax + r9
 	inc rcx							; ++rcx, ready for next call
-	dec r11
-	jmp copy_actual_int						; loop
-
-help:
-	neg rax
+	dec r11							; --r11
+	jmp copy_actual_int				; loop
 
 done:
-	cmp rax, 0
-	jb help
 	pop rbx							; restore rbx
 	mul r8							; multiply rax by our sign
-	mov rsp, rbp
-	pop rbp
+	mov rsp, rbp					; restore stack frame
+	pop rbp							; ^^
 	ret								; return
 
 exit_err:
-	mov rsp, rbp
-	pop rbp
-	xor rax, rax
-	ret
+	mov rsp, rbp					; restore stack frame
+	pop rbp							; ^^
+	xor rax, rax					; set return value to 0
+	ret								; return
